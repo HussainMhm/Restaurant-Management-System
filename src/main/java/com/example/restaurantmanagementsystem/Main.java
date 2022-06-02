5a01f2d3d957bac9a735a2fda15c69f59e077422
Mohammad Salah <83545789+MOSALAH-CODE@users.noreply.github.com>
2022-06-03 02:33:02 +0300

    main

--- src/main/java/com/example/restaurantmanagementsystem/Main.java
+++ src/main/java/com/example/restaurantmanagementsystem/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Order.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("klasmf!");
         stage.setScene(scene);
         stage.show();
     }
