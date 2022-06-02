728f687148e0bf252f96b1ac5df610f755be355a
Hussain Abdullatif <81832286+HussainMhm@users.noreply.github.com>
2022-06-03 01:08:05 +0300

    Revert "Updates"

--- src/main/java/com/example/restaurantmanagementsystem/OrderCtrl.java
+++ src/main/java/com/example/restaurantmanagementsystem/OrderCtrl.java
@@ -1,23 +1,4 @@
 package com.example.restaurantmanagementsystem;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextField;
-
 public class OrderCtrl {
-    @FXML
-    private TextField customerName;
-    @FXML
-    private ChoiceBox<String> foodChoice;
-    @FXML
-    private ChoiceBox<Integer> quantity;
-    @FXML
-    private Button orderBtn;
-    @FXML
-    private TextField total;
-
-    public void order(){
-        System.out.println("ORDER MADE");
-    }
 }
