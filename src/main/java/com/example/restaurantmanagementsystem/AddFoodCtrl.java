605acf25ed70a58ca53f0071981331da49a5d3f0
HussainMhm <hussain.mhmha@gmail.com>
2022-06-02 23:02:45 +0300

    add widgets and functions

--- src/main/java/com/example/restaurantmanagementsystem/AddFoodCtrl.java
+++ src/main/java/com/example/restaurantmanagementsystem/AddFoodCtrl.java
@@ -4,13 +4,15 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
+import static com.example.restaurantmanagementsystem.Kitchen.foodList;
+
 public class AddFoodCtrl {
     @FXML
     private TextField foodName;
     @FXML
-    private TextField foodPrice;
-    @FXML
     private TextField foodCategory;
+    @FXML
+    private TextField foodPrice;
     @FXML
     private Button addFoodBtn;
     // something to upload image
@@ -18,5 +20,16 @@ public class AddFoodCtrl {
 
     public void addFood(){
         System.out.println("Food is added");
+
+        String name = foodName.getText();
+        String category = foodCategory.getText();
+        int price = Integer.parseInt(foodPrice.getText());
+
+        Food newFood = new Food(name, category, price);
+        foodList.add(newFood);
+
+//        System.out.println(foodList.get(1).getName());
+//        System.out.println(foodList.get(1).getPrice());
+//        System.out.println(foodList.get(1).getCategory());
     }
 }
