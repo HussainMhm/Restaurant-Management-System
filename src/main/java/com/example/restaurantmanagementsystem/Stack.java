4772af42a67405afdd2d832d7a89b556823f01b9
Hussain Abdullatif <hussain.mhmha@gmail.com>
2022-06-03 20:36:47 +0300

    last added meals - on zoom

---
+++ src/main/java/com/example/restaurantmanagementsystem/Stack.java
@@ -0,0 +1,60 @@
+package com.example.restaurantmanagementsystem;
+
+public class Stack {
+
+    StackNode root;
+    private int size = 0;
+
+    static class StackNode {
+        Food data;
+        StackNode next;
+
+        StackNode(Food data) { this.data = data; }
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public void push(Food data) {
+        StackNode newNode = new StackNode(data);
+
+        if (root == null) {
+            root = newNode;
+        }
+        else {
+            StackNode temp = root;
+            root = newNode;
+            newNode.next = temp;
+        }
+        System.out.println(data + " pushed to stack");
+        size++;
+    }
+
+    public Food pop() {
+        Food popped = null;
+        if (root == null) {
+            System.out.println("Stack is Empty");
+        }
+        else {
+            popped = root.data;
+            root = root.next;
+        }
+        size--;
+        return popped;
+    }
+
+    public Food peek() {
+        if (root == null) {
+            System.out.println("Stack is empty");
+            return null;
+        }
+        else {
+            return root.data;
+        }
+    }
+
+    public int getSize(){
+        return size;
+    }
+}
