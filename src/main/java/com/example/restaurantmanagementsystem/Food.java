7e92d40b79a66892ac2ec05682746771ec98f616
Hussain Abdullatif <hussain.mhmha@gmail.com>
2022-06-04 03:16:06 +0300

    GUI UPDATE

--- src/main/java/com/example/restaurantmanagementsystem/Food.java
+++ src/main/java/com/example/restaurantmanagementsystem/Food.java
@@ -5,13 +5,10 @@ public class Food {
     private String category;
     private float price;
 
-    // variable for image
-
     public Food(String name, String category, float price) {
         this.name = name;
         this.category = category;
         this.price = price;
-        // this.image also
     }
 
     public String getName() {
